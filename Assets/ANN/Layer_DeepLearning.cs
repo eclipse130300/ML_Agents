cb099541d87c8c1c0fc825bf2e25fc7d7d834bc7
eclipse130300 <eclipse130300@yandex.ru>
2024-11-14 04:01:44 +0300

    Added ball balance scene

---
+++ Assets/ANN/Layer_DeepLearning.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer_DeepLearning {
+
+	public int numNeurons;
+	public List<Neuron_DeepLearning> neurons = new List<Neuron_DeepLearning>();
+
+	public Layer_DeepLearning(int nNeurons, int numNeuronInputs)
+	{
+		numNeurons = nNeurons;
+		for(int i = 0; i < nNeurons; i++)
+		{
+			neurons.Add(new Neuron_DeepLearning(numNeuronInputs));
+		}
+	}
+}
