4835e42c6de7143bfbd1b2873619bb6668b9a7c0
eclipse130300 <eclipse130300@yandex.ru>
2024-11-19 04:44:27 +0300

    ball balance finished

--- Assets/PlatformBrain.cs
+++ Assets/PlatformBrain.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using NeuralNetworkToolkit;
 using UnityEngine;
+using UnityStandardAssets.Vehicles.Ball;
 
 public class Replay
 {
@@ -20,30 +23,159 @@ public class Replay
 
 public class PlatformBrain : MonoBehaviour
 {
-
+    [Range(1, 100f)]
+    public float timeScale = 100.0f;
     public GameObject ball;
-    
+
     ANN_DeepLearning ann;
-    
+
     float reward = 0.0f;
     List<Replay> replayMemory = new List<Replay>();
     int mCapacity = 10000;
-    
+
     float discount = 0.99f;
-    float exploreRate = 100.0f;
+    float exploreRate = 50.0f;
     float maxExploreRate = 100.0f;
     float minExploreRate = 0.01f;
     float exploreDecay = 0.0001f;
-    
+
     Vector3 ballStartPosition;
     int failCount = 0;
     float tiltSpeed = 0.5f;
-    
+
     float timer = 0;
     float maxBalanceTime = 0;
 
     private void Start()
     {
+        ann = new ANN_DeepLearning(3, 2, 1, 6, 0.5f, ActivationFunctionType.Sigmoid, ActivationFunctionType.Sigmoid);
+        ballStartPosition = ball.transform.position;
+        //Time.timeScale = 100.0f;
+    }
+
+    GUIStyle guiStyle = new GUIStyle();
+
+    private void OnGUI()
+    {
+        guiStyle.fontSize = 25;
+        guiStyle.normal.textColor = Color.white;
+        GUI.BeginGroup(new Rect(10, 10, 250, 150));
+        GUI.Box(new Rect(0, 0, 400, 140), "Stats", guiStyle);
+        GUI.Label(new Rect(10, 25, 400, 30), "Fails: " + failCount, guiStyle);
+        GUI.Label(new Rect(10, 50, 400, 30), "Decay Rate: " + exploreRate, guiStyle);
+        GUI.Label(new Rect(10, 75, 400, 30), "Last B Balance: " + maxBalanceTime, guiStyle);
+        GUI.Label(new Rect(10, 100, 400, 30), "This Balance: " + timer, guiStyle);
+        GUI.EndGroup();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+            ResetBall();
         
+        Time.timeScale = timeScale;
+    }
+
+    private void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        List<double> states = new List<double>();
+        List<double> qs = new List<double>();
+
+        states.Add(transform.position.x);
+        states.Add(ball.transform.position.z);
+        states.Add(ball.GetComponent<Rigidbody>().angularVelocity.x);
+
+        qs = SoftMax(ann.CalcOutput(states));
+        double maxQ = qs.Max();
+        int maxQIndex = qs.ToList().IndexOf(maxQ);
+        exploreRate = Mathf.Clamp(exploreRate - exploreDecay, minExploreRate, maxExploreRate);
+
+        if (UnityEngine.Random.Range(0, 100) < exploreRate)
+            maxQIndex = UnityEngine.Random.Range(0, 2);
+
+        var qsMax = qs[maxQIndex];
+        if(qsMax == 0f)
+            Debug.Log("ZERO!? ");
+
+        if (maxQIndex == 0)
+            transform.Rotate(Vector3.right, tiltSpeed * (float)qs[maxQIndex]);
+        else if (maxQIndex == 1)
+            transform.Rotate(Vector3.right, -tiltSpeed * (float)qs[maxQIndex]);
+
+        if (ball.GetComponent<BallState>().dropped)
+        {
+            reward = -1.0f;
+        }
+        else
+        {
+            reward = 0.1f;
+        }
+
+        Replay lastMemory = new Replay(transform.position.x, ball.transform.position.z, ball.GetComponent<Rigidbody>().angularVelocity.x, reward);
+        if (replayMemory.Count > mCapacity)
+            replayMemory.RemoveAt(0);
+
+        replayMemory.Add(lastMemory);
+
+        if (ball.GetComponent<BallState>().dropped)
+        {
+            for (int i = replayMemory.Count - 1; i >= 0; i--)
+            {
+                List<double> toutputsOld = new List<double>();
+                List<double> toutputsNew = new List<double>();
+                toutputsOld = SoftMax(ann.CalcOutput(replayMemory[i].states));
+
+                double maxQOld = toutputsOld.Max();
+                int action = toutputsOld.ToList().IndexOf(maxQOld);
+
+                double feedback;
+                if (i == replayMemory.Count - 1 || replayMemory[i].reward == -1)
+                    feedback = replayMemory[i].reward;
+                else
+                {
+                    toutputsNew = SoftMax(ann.CalcOutput(replayMemory[i + 1].states));
+                    maxQ = toutputsNew.Max();
+                    feedback = (replayMemory[i].reward + discount * maxQ);
+                }
+
+                toutputsOld[action] = feedback;
+                ann.Train(replayMemory[i].states, toutputsOld);
+            }
+
+            if (timer > maxBalanceTime)
+                maxBalanceTime = timer;
+
+            timer = 0;
+
+            ball.GetComponent<BallState>().dropped = false;
+            transform.rotation = Quaternion.identity;
+            ResetBall();
+            replayMemory.Clear();
+            failCount++;
+        }
+    }
+
+    private void ResetBall()
+    {
+        ball.transform.position = ballStartPosition;
+        ball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        ball.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+    }
+    
+    private List<double> SoftMax(List<double> values)
+    {
+        double max = values.Max();
+        float scale = 0.0f;
+
+        for (int i = 0; i < values.Count; i++)
+        {
+            scale += (float)Math.Exp(values[i] - max);
+        }
+        
+        List<double> result = new List<double>();
+        for (int i = 0; i < values.Count; i++)
+            result.Add(Math.Exp(values[i] - max) / scale);
+        return result;
     }
 }
